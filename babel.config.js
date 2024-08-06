f4d6a8ec905a461d666ad1e0a7de61fa37a64201
natchalux <natchaluxw@gmail.com>
2024-08-06 15:54:42 +0100

    deps

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,14 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo','@babel/preset-react','module:metro-react-native-babel-preset',],
-    plugins: ['nativewind/babel','react-native-reanimated/plugin'],
-    
+    presets: [
+      'babel-preset-expo',
+      '@babel/preset-react',
+      ['module:metro-react-native-babel-preset', { useTransformReactJSXExperimental: true }]
+    ],
+    plugins: [
+      'nativewind/babel',
+      'react-native-reanimated/plugin',
+    ]
   };
 };
